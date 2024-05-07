61645e1ea599ad2c659b4f9a2310d023a708df03
aniketgoyal51 <aniketgoyal791@gmail.com>
2024-05-07 14:42:08 +0530

    updtaed the update form according to interrier dates

--- client/src/Components/UpdateCustomerDetails.jsx
+++ client/src/Components/UpdateCustomerDetails.jsx
@@ -5,32 +5,36 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 
 function UpdateCustomerDetails() {
     const [Name, setName] = useState("")
-    const [Email, setEmail] = useState("")
     const [Phone, setPhone] = useState("")
     const [Address, setAddress] = useState("")
-    const [Car, setCar] = useState("")
     const [Package, setPackage] = useState("")
+    const [interrierfirst, setinterrierfirst] = useState("")
+    const [interriersecond, setinterriersecond] = useState("")
+    const [interrierthird, setinterrierthird] = useState("")
+    const [interrierfourth, setinterrierfourth] = useState("")
     const [StartDate, setDate] = useState([])
-    const navigate=useNavigate()
-    const id=useParams().id
+    const navigate = useNavigate()
+    const id = useParams().id
 
     useEffect(() => {
         axios.get(`${import.meta.env.VITE_SERVER_URL}/getdatabyid/${id}`)
             .then(res => {
                 setName(res.data.Name)
-                setEmail(res.data.Email)
                 setPhone(res.data.Phone)
                 setAddress(res.data.Address)
-                setCar(res.data.Car)
                 setPackage(res.data.Package)
+                setinterrierfirst(res.data.interrierfirst)
+                setinterriersecond(res.data.interriersecond)
+                setinterrierthird(res.data.interrierthird)
+                setinterrierfourth(res.data.interrierfourth)
                 setDate(res.data.StartDate.split("T")[0])
             })
             .catch(err => console.log(err))
-    },[])
+    }, [])
 
     const onSubmit = (data) => {
         // console.log(data)
-        axios.put(`${import.meta.env.VITE_SERVER_URL}/update/${id}`, {Name, Email, Phone, Address, Car, Package, StartDate})
+        axios.put(`${import.meta.env.VITE_SERVER_URL}/update/${id}`, { Name, Phone, Address, Package, StartDate,interrierfirst,interriersecond,interrierthird,interrierfourth })
             .then(res => {
                 // console.log(res.data)
                 navigate("/CustomerDetails")
@@ -43,15 +47,11 @@ function UpdateCustomerDetails() {
             <Link to="/"><button type="button">Home</button></Link>
             <div>
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="Name" defaultValue={Name} onChange={(e) => setName(e.target.value)}/>
-                <label htmlFor="email">Email</label>
-                <input type="text" id="email" name="Email" defaultValue={Email} onChange={(e) => setEmail(e.target.value)}/>
+                <input type="text" id="name" name="Name" defaultValue={Name} onChange={(e) => setName(e.target.value)} />
                 <label htmlFor="phone">Phone</label>
-                <input type="text" id="phone" name="Phone" defaultValue={Phone} onChange={(e) => setPhone(e.target.value)}/>
+                <input type="text" id="phone" name="Phone" defaultValue={Phone} onChange={(e) => setPhone(e.target.value)} />
                 <label htmlFor="address">Address</label>
-                <input type="text" id="address" name="Address" defaultValue={Address} onChange={(e) => setAddress(e.target.value)}/>
-                <label htmlFor="car">Car</label>
-                <input type="text" id="car" name="Car" defaultValue={Car} onChange={(e) => setCar(e.target.value)}/>
+                <input type="text" id="address" name="Address" defaultValue={Address} onChange={(e) => setAddress(e.target.value)} />
                 <label htmlFor="package">Package</label>
                 <select name="Package" id="" value={Package} onChange={(e) => setPackage(e.target.value)}>
                     <option value="650 (Hatchback)" >650 (Hatchback)</option>
@@ -60,7 +60,25 @@ function UpdateCustomerDetails() {
                     <option value="850 (luxury)">850 (luxury)</option>
                 </select>
                 <label htmlFor="Date">Start Date</label>
-                <input type="date" id="Date" name="Date" defaultValue={StartDate} onChange={(e) => setDate(e.target.value)}/>
+                <input type="date" id="Date" name="Date" defaultValue={StartDate} onChange={(e) => setDate(e.target.value)} />
+                <div>
+                    <div>
+                        <label>Interrie First</label>
+                        <input type="date" id="Date" name="Date" defaultValue={interrierfirst} onChange={(e) => setinterrierfirst(e.target.value)} />
+                    </div>
+                    <div>
+                        <label>Interrie Second</label>
+                        <input type="date" id="Date" name="Date" defaultValue={interriersecond} onChange={(e) => setinterriersecond(e.target.value)} />
+                    </div>
+                    <div>
+                        <label>Interrie Third</label>
+                        <input type="date" id="Date" name="Date" defaultValue={interrierthird} onChange={(e) => setinterrierthird(e.target.value)} />
+                    </div>
+                    <div>
+                        <label>Interrie Fourth</label>
+                        <input type="date" id="Date" name="Date" defaultValue={interrierfourth} onChange={(e) => setinterrierfourth(e.target.value)} />
+                    </div>
+                </div>
                 <button type='button' onClick={() => navigate(-1)}>Back</button>
                 <button type='submit' onClick={() => onSubmit()}>Submit</button>
             </div>
